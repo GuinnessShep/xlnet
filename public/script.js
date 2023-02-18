3cb6cd16db61d5e12843dc5c2c5bbe3e271ce87e
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-18 07:52:55 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,35 @@
+/* script.js */
+
 const chat = document.querySelector('.chat');
+const input = document.querySelector('input[type="text"]');
+const container = document.querySelector('.container');
+const sendButton = document.querySelector('button');
 
 function addMessage(text, sender) {
-	const message = document.createElement('div');
-	message.classList.add('message');
-	message.classList.add(sender);
+  const message = document.createElement('div');
+  message.classList.add('message');
+  message.classList.add(sender);
 
-	const content = document.createElement('p');
-	content.textContent = text;
+  const content = document.createElement('p');
+  content.textContent = text;
 
-	message.appendChild(content);
-	chat.appendChild(message);
+  message.appendChild(content);
+  chat.appendChild(message);
 }
 
 addMessage('Hello, how can I help you?', 'bot');
+
+function toggleDarkMode() {
+  container.classList.toggle('dark');
+  input.classList.toggle('dark');
+  sendButton.classList.toggle('dark');
+}
+
+function toggleAlternateBackground() {
+  container.classList.toggle('alternate');
+  input.classList.toggle('alternate');
+  sendButton.classList.toggle('alternate');
+}
+
+document.querySelector('#darkMode').addEventListener('change', toggleDarkMode);
+document.querySelector('#alternateBackground').addEventListener('change', toggleAlternateBackground);
