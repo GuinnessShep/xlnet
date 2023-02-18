b026a96d27d314f1682a3433c4b919f13ad6f0b8
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-17 20:29:35 -0800

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,18 @@
-const query = require('./xnet');
+const fetch = require("node-fetch");
+
+async function query(data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/xlnet-base-cased",
+    {
+      headers: { Authorization: "Bearer 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" },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
 
 module.exports = {
-  query
+  query: query,
 };
