3636900cee130037d8fd7e91f81125c6438f0546
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-18 08:06:17 -0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,35 +1,70 @@
-/* script.js */
-
 const chat = document.querySelector('.chat');
 const input = document.querySelector('input[type="text"]');
-const container = document.querySelector('.container');
 const sendButton = document.querySelector('button');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const backgroundUpload = document.getElementById('background-upload');
+const customizationPanel = document.querySelector('.customization-panel');
 
 function addMessage(text, sender) {
-  const message = document.createElement('div');
-  message.classList.add('message');
-  message.classList.add(sender);
+	const message = document.createElement('div');
+	message.classList.add('message');
+	message.classList.add(sender);
 
-  const content = document.createElement('p');
-  content.textContent = text;
+	const content = document.createElement('p');
+	content.textContent = text;
 
-  message.appendChild(content);
-  chat.appendChild(message);
+	message.appendChild(content);
+	chat.appendChild(message);
 }
 
-addMessage('Hello, how can I help you?', 'bot');
+sendButton.addEventListener('click', () => {
+	if (input.value) {
+		addMessage(input.value, 'me');
+		input.value = '';
+		setTimeout(() => addMessage('I am a chatbot', 'bot'), 1000);
+	}
+});
 
-function toggleDarkMode() {
-  container.classList.toggle('dark');
-  input.classList.toggle('dark');
-  sendButton.classList.toggle('dark');
-}
+input.addEventListener('keydown', (event) => {
+	if (event.key === 'Enter' && input.value) {
+		addMessage(input.value, 'me');
+		input.value = '';
+		setTimeout(() => addMessage('I am a chatbot', 'bot'), 1000);
+	}
+});
 
-function toggleAlternateBackground() {
-  container.classList.toggle('alternate');
-  input.classList.toggle('alternate');
-  sendButton.classList.toggle('alternate');
-}
+darkModeToggle.addEventListener('change', () => {
+	document.body.classList.toggle('dark-mode');
+});
 
-document.querySelector('#darkMode').addEventListener('change', toggleDarkMode);
-document.querySelector('#alternateBackground').addEventListener('change', toggleAlternateBackground);
+backgroundUpload.addEventListener('change', () => {
+	const file = backgroundUpload.files[0];
+	const reader = new FileReader();
+
+	reader.addEventListener('load', () => {
+		document.body.style.backgroundImage = `url(${reader.result})`;
+	});
+
+	if (file) {
+		reader.readAsDataURL(file);
+	}
+});
+
+customizationPanel.addEventListener('click', (event) => {
+	event.stopPropagation();
+});
+
+document.addEventListener('click', () => {
+	customizationPanel.classList.remove('open');
+});
+
+document.addEventListener('keydown', (event) => {
+	if (event.key === 'Escape') {
+		customizationPanel.classList.remove('open');
+	}
+});
+
+document.querySelector('.container').addEventListener('click', (event) => {
+	event.stopPropagation();
+	customizationPanel.classList.toggle('open');
+});
