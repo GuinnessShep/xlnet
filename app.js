bcafb37c46729fbbe5410c5c3b520ba8f6c68e6b
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-17 20:15:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.post("/", async (req, res) => {
   res.send(result);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
